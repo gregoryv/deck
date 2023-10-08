5e7ab519751e5696678239534a9badf9aef84d80
Gregory Vincic <gregoryvse@gmail.com>
2023-10-08 05:16:36 +0200

    Tidy

--- example_test.go
+++ example_test.go
@@ -9,7 +9,6 @@ func Example_pack() {
 	p := deck.NewPack()
 	p.Title = "My funny presentation"
 	p.Author = "Gregory Vinčić"
-	p.Style(".nowrap", "white-space: nowrap")
 	// dark mode
 	//p.Style("html, body", "background-color: #2E2E34", "color: #f0f8ff")
 
@@ -17,13 +16,13 @@ func Example_pack() {
 		H2("Animals"),
 		deck.Double(
 			lorem,
-			ipsum(),
+			ipsum,
 		),
 	)
 	p.NewSlide(
 		H3("Horses"),
-		deck.Middle(69,
-			ipsum().With(Class("nowrap")),
+		deck.Middle(50,
+			ipsum,
 		),
 	)
 	p.NewSlide(
@@ -56,15 +55,13 @@ in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
 culpa qui officia deserunt mollit anim id est laborum.`)
 
-func ipsum() *Element {
-	return Ul(
-		Li("Lorem ipsum dolor sit amet, consectetur adipiscing elit,"),
-		Li("sed do eiusmod tempor incididunt ut labore et dolore magna"),
-		Li("aliqua. Ut enim ad minim veniam, quis nostrud exercitation"),
-		Li("ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis"),
-		Li("aute irure dolor in reprehenderit in voluptate velit esse"),
-		Li("cillum dolore eu fugiat nulla pariatur. Excepteur sint"),
-		Li("occaecat cupidatat non proident, sunt in culpa qui officia"),
-		Li("deserunt mollit anim id est laborum."),
-	)
-}
+var ipsum = Ul(
+	Li("Lorem ipsum dolor sit amet, consectetur adipiscing elit,"),
+	Li("sed do eiusmod tempor incididunt ut labore et dolore magna"),
+	Li("aliqua. Ut enim ad minim veniam, quis nostrud exercitation"),
+	Li("ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis"),
+	Li("aute irure dolor in reprehenderit in voluptate velit esse"),
+	Li("cillum dolore eu fugiat nulla pariatur. Excepteur sint"),
+	Li("occaecat cupidatat non proident, sunt in culpa qui officia"),
+	Li("deserunt mollit anim id est laborum."),
+)
